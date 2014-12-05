cacbe5c05f93f27e842a44b0a9db5a9a695bcb5c
brentmc <brent@intrepica.com.au>
2014-12-05 11:43:13 +1000

    HW01 CreditCard - code cleanup

--- HW01CreditCard.hs
+++ HW01CreditCard.hs
@@ -1,18 +1,8 @@
 {-
-Name: <your name here>
-Collaborators: <your collaborators here, or "none">
-Notes: <any particular notes about your work -- what you struggled with,
-        what's not working, what's really cool, etc.>
+Name: Brent McIvor
 -}
 
-module HW01 where         -- We'll learn more about this later
-
-isThisWorking :: String
-isThisWorking = "Yes"
--- Load this file into GHCi (say, with `ghci HW01.hs`) and type
--- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
-
--- Put your work below.
+module HW01 where
 
 lastDigit :: Integer -> Integer
 lastDigit n = n `mod` 10
@@ -20,25 +10,20 @@ lastDigit n = n `mod` 10
 dropLastDigit :: Integer -> Integer
 dropLastDigit n = n `div` 10
 
-
 toDigits :: Integer -> [Integer]
 toDigits n 
    | n < 1    = []
    | otherwise = toDigits (dropLastDigit n) ++ [lastDigit n]
-
-
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x:[]) = [x]
 doubleEveryOther (x:y:zs) = reverse (double2nd (reverse (x:y:zs))) 
 
-
 double2nd :: [Integer] -> [Integer]
 double2nd [] = []
 double2nd (x:[]) = [x]
 double2nd (x:y:zs) = x : double y : double2nd zs 
-
 
 double :: Integer -> Integer
 double n = n * 2
