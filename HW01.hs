6a4682d6227d34e1c141dd9ea88344a5f9367212
brentmc <brent@intrepica.com.au>
2014-12-05 11:27:02 +1000

    HW01 - Exercise 4 - sumDigits

--- HW01.hs
+++ HW01.hs
@@ -42,3 +42,15 @@ double2nd (x:y:zs) = x : double y : double2nd zs
 
 double :: Integer -> Integer
 double n = n * 2
+
+--sums all digits in a list of Integers
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits [x] = x
+sumDigits (x:y:zs) = sumAllDigits x + sumAllDigits y +  sumDigits zs
+
+--sums all of the digits in a single Integer
+sumAllDigits :: Integer -> Integer
+sumAllDigits n
+   | n < 1 = 0
+   | otherwise = lastDigit n + sumAllDigits(dropLastDigit n)
