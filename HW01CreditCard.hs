a4aa5482d94cb02e02b75402ddd11367678fd136
brentmc <brent@intrepica.com.au>
2014-12-05 14:47:34 +1000

    HW01 CreditCard - Added comments

--- HW01CreditCard.hs
+++ HW01CreditCard.hs
@@ -10,16 +10,24 @@ lastDigit n = n `mod` 10
 dropLastDigit :: Integer -> Integer
 dropLastDigit n = n `div` 10
 
+--converts the given Integer to a list of its digits
 toDigits :: Integer -> [Integer]
 toDigits n 
    | n < 1    = []
    | otherwise = toDigits (dropLastDigit n) ++ [lastDigit n]
 
+{-
+doubles every second digit beginning from the right
+i.e. the last digit is unchanged, 
+the second last is doubled, 
+the third last is unchanged
+-}
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x:[]) = [x]
 doubleEveryOther (x:y:zs) = reverse (double2nd (reverse (x:y:zs))) 
 
+--doubles every second element in the given list
 double2nd :: [Integer] -> [Integer]
 double2nd [] = []
 double2nd (x:[]) = [x]
@@ -29,16 +37,17 @@ double :: Integer -> Integer
 double n = n * 2
 
 --sums all digits in a list of Integers
-sumDigits :: [Integer] -> Integer
-sumDigits [] = 0
-sumDigits [x] = x
-sumDigits (x:y:zs) = sumAllDigits x + sumAllDigits y +  sumDigits zs
+sumAllDigitsInList :: [Integer] -> Integer
+sumAllDigitsInList [] = 0
+sumAllDigitsInList [x] = x
+sumAllDigitsInList (x:y:zs) = sumDigitsInInteger x + sumDigitsInInteger y +  sumAllDigitsInList zs
 
 --sums all of the digits in a single Integer
-sumAllDigits :: Integer -> Integer
-sumAllDigits n
+sumDigitsInInteger :: Integer -> Integer
+sumDigitsInInteger n
    | n < 1 = 0
-   | otherwise = lastDigit n + sumAllDigits(dropLastDigit n)
+   | otherwise = lastDigit n + sumDigitsInInteger(dropLastDigit n)
 
+--determines if the given Integer is a valid credit card number
 validate :: Integer -> Bool
-validate n = (sumDigits  (doubleEveryOther (toDigits n))) `mod` 10 == 0
+validate n = (sumAllDigitsInList  (doubleEveryOther (toDigits n))) `mod` 10 == 0
