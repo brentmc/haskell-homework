f6a79f95b81e9e45a6cf79714f2a66f3f6a7b499
brentmc <brent@intrepica.com.au>
2014-12-05 14:54:24 +1000

    HW01 - CreditCard - renamed the function

--- HW01CreditCard.hs
+++ HW01CreditCard.hs
@@ -22,16 +22,21 @@ i.e. the last digit is unchanged,
 the second last is doubled, 
 the third last is unchanged
 -}
-doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther [] = []
-doubleEveryOther (x:[]) = [x]
-doubleEveryOther (xs) = reverse (double2nd (reverse (xs))) 
+doubleEveryOtherFromRight :: [Integer] -> [Integer]
+doubleEveryOtherFromRight [] = []
+doubleEveryOtherFromRight (x:[]) = [x]
+doubleEveryOtherFromRight (xs) = reverse (doubleEveryOtherFromLeft (reverse (xs))) 
 
---doubles every second element in the given list
-double2nd :: [Integer] -> [Integer]
-double2nd [] = []
-double2nd (x:[]) = [x]
-double2nd (x:y:zs) = x : double y : double2nd zs 
+{-
+doubles every second digit beginning from the left
+i.e. the first digit is unchanged, 
+the second is doubled, 
+the third is unchanged
+-}
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft [] = []
+doubleEveryOtherFromLeft (x:[]) = [x]
+doubleEveryOtherFromLeft (x:y:zs) = x : double y : doubleEveryOtherFromLeft zs 
 
 double :: Integer -> Integer
 double n = n * 2
@@ -50,4 +55,4 @@ sumDigitsInInteger n
 
 --determines if the given Integer is a valid credit card number
 validate :: Integer -> Bool
-validate n = (sumAllDigitsInList  (doubleEveryOther (toDigits n))) `mod` 10 == 0
+validate n = (sumAllDigitsInList  (doubleEveryOtherFromRight (toDigits n))) `mod` 10 == 0
